9ba49480ea9eeba140a9393f4b592acd862c2905
Dominic Lynch <dom_lynch@yahoo.com>
2018-04-17 22:07:56 +0100

    initial commit

---
+++ src/uk/gov/snh/quote/Quote.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.gov.snh.quote;
+
+/**
+ * @author INDL
+ *
+ */
+public class Quote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Chuck Norris' tears cure cancer. Too bad he has never cried. Ever.");
+	}
+
+}
